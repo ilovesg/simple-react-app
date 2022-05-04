e93693a547bc4221381e6fe7a8d02b4e1a151ed5
ilovesg <immortalbeing13@gmail.com>
2022-05-04 19:09:25 +0500

    Import PostList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Counter from './features/counter/Counter';
 import './App.scss';
+import PostsList from './features/posts/PostList';
 
 function App() {
   return (
     <Container>
-      <Counter />
+      <PostsList />
     </Container>
   );
 }
