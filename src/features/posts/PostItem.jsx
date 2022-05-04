345bbe3b5dea76e0f7d2b3079f35ff7772579c51
ilovesg <immortalbeing13@gmail.com>
2022-05-04 22:26:02 +0500

    Add post items output

--- src/features/posts/PostItem.jsx
+++ src/features/posts/PostItem.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ListGroup, Button } from 'react-bootstrap';
 
-export default function PostItem() {
+export default function PostItem({ post }) {
   return (
-    <div>Post item is here.</div>
+    <ListGroup.Item
+      as="li"
+      className="d-flex justify-content-between align-items-center"
+    >
+      <div className="ms-2 me-auto">
+        <h5 className="fw-bold">
+          {post.title}
+        </h5>
+        {post.body}
+      </div>
+      <Button variant="danger">&times; Delete</Button>
+    </ListGroup.Item>
   );
 }
--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ListGroup } from 'react-bootstrap';
+import PostItem from './PostItem';
 
 export default function PostsList() {
+  const posts = useSelector((state) => state.posts.posts);
+
   return (
     <div className="post-list">
       <h1>Post list</h1>
-      <div>Posts are there.</div>
+      <ListGroup as="ul">
+        {
+          posts.map((post) => (
+            <PostItem post={post} key={post.id} />
+          ))
+        }
+      </ListGroup>
     </div>
   );
 }
