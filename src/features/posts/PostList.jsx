88eacc54e2209fed9682f019167600af33c76330
ilovesg <immortalbeing13@gmail.com>
2022-05-04 19:08:35 +0500

    Create PostList component

---
+++ src/features/posts/PostList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PostsList() {
+  return (
+    <div className="post-list">
+      <h1>Post list</h1>
+      <div>Posts are there.</div>
+    </div>
+  );
+}
