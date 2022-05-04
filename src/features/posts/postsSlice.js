681cac63d7e45667e5119a14d3f27d32d0ee68b0
ilovesg <immortalbeing13@gmail.com>
2022-05-04 20:12:31 +0500

    Create posts slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  posts: [
+    {
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      id: 2,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      id: 3,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+  ],
+  status: 'idle',
+};
+
+export const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    addPost: (state) => {
+      state.value += 1;
+    },
+    removePost: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { addPost, removePost } = postsSlice.actions;
+
+export default postsSlice.reducer;
