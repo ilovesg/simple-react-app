09746178056d5f72b1453bda6cbfa4fed3b3213a
ilovesg <immortalbeing13@gmail.com>
2022-07-18 00:28:46 +0500

    Add basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import './App.scss';
 import { ToastContainer, toast } from 'react-toastify';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Posts from './features/posts/Posts';
+import About from './pages/About';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <Container>
       <ToastContainer />
-      <Posts toast={toast} />
+      <Routes>
+        <Route index path="/" element={<Posts toast={toast} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </Container>
   );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function PageNotFound() {
+  return (
+    <p>
+      Page not found.
+      {' '}
+      <a href="/">Go back to the main page</a>
+    </p>
+  );
+}
