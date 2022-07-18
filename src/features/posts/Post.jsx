2b327488e2d3f270f72913ced5a5107654596791
ilovesg <immortalbeing13@gmail.com>
2022-07-18 23:47:03 +0500

    Display single post data

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetPostQuery } from './postsAPI';
+import Loader from '../loader/Loader';
 
 export default function Post() {
+  const { id } = useParams();
+
+  const {
+    data,
+    isLoading,
+  } = useGetPostQuery(id);
+
   return (
     <div>
-      Single post is here.
+      {!isLoading ? (
+        <article className="text-center">
+          <h1>{`Post ${data.id}: ${data.title}`}</h1>
+          <p>{data.body}</p>
+          <span>{`Author ID: ${data.userId}`}</span>
+        </article>
+      )
+        : <Loader />}
     </div>
   );
 }
