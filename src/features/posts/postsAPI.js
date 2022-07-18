8735637a70c27d30c6a3a41dffff1859c811cc0b
ilovesg <immortalbeing13@gmail.com>
2022-07-18 16:40:57 +0500

    Switch to JSONPlaceholder API

--- src/features/posts/postsAPI.js
+++ src/features/posts/postsAPI.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const postsApi = createApi({
   reducerPath: 'postsApi',
   tagTypes: ['Posts'],
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: (page = 1, limit = 10) => `posts?_page=${page}&_limit=${limit}`,
