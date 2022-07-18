372f456843bf150e8ff0ad363a0468ed415502d5
ilovesg <immortalbeing13@gmail.com>
2022-07-18 15:22:32 +0500

    Add navbar

---
+++ src/features/navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+export default function Navigation() {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Simple React App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
