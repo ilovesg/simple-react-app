d094d8e36cf877b2b37b6e8829bbb59e7f3f2cb1
ilovesg <immortalbeing13@gmail.com>
2022-07-19 14:24:39 +0500

    Set relative link to the single post page

--- src/features/posts/PostList/PostTable/PostItem/PostItem.jsx
+++ src/features/posts/PostList/PostTable/PostItem/PostItem.jsx
@@ -32,7 +32,7 @@ export default function PostItem({ post }) {
   return (
     <tr>
       <td>{post.id}</td>
-      <td><Link to={`posts/${post.id}`}>{post.title}</Link></td>
+      <td><Link to={`${post.id}`}>{post.title}</Link></td>
       <td>{post.body}</td>
       <td>
         <Button variant="danger" onClick={() => deletePostHandler(post.id)} disabled={isLoading}>
