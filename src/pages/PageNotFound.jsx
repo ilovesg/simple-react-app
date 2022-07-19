fdd08f566f08f6114020af85c24150b02fdcb14b
ilovesg <immortalbeing13@gmail.com>
2022-07-19 14:23:22 +0500

    Add some text and link to the post page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default function PageNotFound() {
   return (
-    <p>
-      Page not found.
-      {' '}
-      <Link to="/">Go back to the main page</Link>
-    </p>
+    <div>
+      <h1>Page not found</h1>
+      <p>
+        We apologize for any inconvenience caused.
+        {' '}
+        <Link to="/">Go back to the homepage</Link>
+        {' '}
+        or
+        {' '}
+        <Link to="posts">to the post page</Link>
+      </p>
+    </div>
   );
 }
