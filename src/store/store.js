2fe096c693dcdb6f8e74a4ac28a75d964341c11f
ilovesg <immortalbeing13@gmail.com>
2022-07-19 15:26:42 +0500

    Connect app slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import postsApi from '../features/posts/postsAPI';
 import postsReducer from '../features/posts/postsSlice';
+import appReducer from '../app/appSlice';
 
 const store = configureStore({
   reducer: {
     [postsApi.reducerPath]: postsApi.reducer,
     posts: postsReducer,
+    app: appReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     postsApi.middleware,
