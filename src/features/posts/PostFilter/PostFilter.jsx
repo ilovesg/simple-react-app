443a97a65f7fc8ce5d92264cf549e454f800b37d
ilovesg <immortalbeing13@gmail.com>
2022-07-21 16:41:09 +0500

    Replace Bootsrap row and column layout with div

--- src/features/posts/PostFilter/PostFilter.jsx
+++ src/features/posts/PostFilter/PostFilter.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  Row,
-  Col,
   Form,
   Dropdown,
   DropdownButton,
@@ -18,15 +16,13 @@ export default function PostFilter() {
   }, [dispatch, filter]);
 
   return (
-    <Row className="mb-3">
-      <Col>
-        <h2>Search post</h2>
-        <DropdownButton id="dropdown-basic-button" title={`Search by: ${filter.filterBy}`} className="mb-3">
-          <Dropdown.Item onClick={() => setFilter({ ...filter, filterBy: 'title' })}>Title</Dropdown.Item>
-          <Dropdown.Item onClick={() => setFilter({ ...filter, filterBy: 'body' })}>Body</Dropdown.Item>
-        </DropdownButton>
-        <Form.Control type="text" placeholder="Search post.." value={filter.filterQuery} onChange={(event) => setFilter({ ...filter, filterQuery: event.target.value })} />
-      </Col>
-    </Row>
+    <div className="mb-3">
+      <h2>Search post</h2>
+      <DropdownButton id="dropdown-basic-button" title={`Search by: ${filter.filterBy}`} className="mb-3">
+        <Dropdown.Item onClick={() => setFilter({ ...filter, filterBy: 'title' })}>Title</Dropdown.Item>
+        <Dropdown.Item onClick={() => setFilter({ ...filter, filterBy: 'body' })}>Body</Dropdown.Item>
+      </DropdownButton>
+      <Form.Control type="text" placeholder="Search post.." value={filter.filterQuery} onChange={(event) => setFilter({ ...filter, filterQuery: event.target.value })} />
+    </div>
   );
 }
